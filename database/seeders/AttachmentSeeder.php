9f214e13bbb07b5e53a27f89283b553d15ef3ff3
amer <amer23zx@gmail.com>
2021-12-09 16:11:29 +0800

    Add More  seeder data

--- database/seeders/AttachmentSeeder.php
+++ database/seeders/AttachmentSeeder.php
@@ -23,11 +23,20 @@ class AttachmentSeeder extends Seeder
          array('id' => '2','attachable_id'=>'1','attachable_type'=>'project','title'=>'Project image','link'=>'https://image.freepik.com/free-vector/business-start-up-concept-illustration_107791-86.jpg?1','type'=>'photo',),
          array('id' => '3','attachable_id'=>'1','attachable_type'=>'project','title'=>'Project image','link'=>'https://image.freepik.com/free-vector/business-start-up-concept-illustration_107791-86.jpg?1','type'=>'photo',),
          array('id' => '4','attachable_id'=>'1','attachable_type'=>'project','title'=>'Project image','link'=>'https://image.freepik.com/free-vector/business-start-up-concept-illustration_107791-86.jpg?1','type'=>'photo',),
-         array('id' => '5','attachable_id'=>'1','attachable_type'=>'project','title'=>'profile image','link'=>'https://media-exp1.licdn.com/dms/image/C5103AQHl49hQvQeKGg/profile-displayphoto-shrink_200_200/0/1566916027452?e=1644451200&v=beta&t=jKSOYeYW_eGThVMKZ2UsSBPzWat99LgcIMfbmWcXLeQ','type'=>'photo',),
+         array('id' => '5','attachable_id'=>'1','attachable_type'=>'profile','title'=>'profile image','link'=>'https://media-exp1.licdn.com/dms/image/C5103AQHl49hQvQeKGg/profile-displayphoto-shrink_200_200/0/1566916027452?e=1644451200&v=beta&t=jKSOYeYW_eGThVMKZ2UsSBPzWat99LgcIMfbmWcXLeQ','type'=>'photo',),
          array('id' => '6','attachable_id'=>'2','attachable_type'=>'project','title'=>'Project image','link'=>'https://image.freepik.com/free-vector/business-start-up-concept-illustration_107791-86.jpg?1','type'=>'photo',),
          array('id' => '7','attachable_id'=>'2','attachable_type'=>'project','title'=>'Project image','link'=>'https://image.freepik.com/free-vector/business-start-up-concept-illustration_107791-86.jpg?1','type'=>'photo',),
          array('id' => '8','attachable_id'=>'2','attachable_type'=>'project','title'=>'Project image','link'=>'https://image.freepik.com/free-vector/business-start-up-concept-illustration_107791-86.jpg?1','type'=>'photo',),
          array('id' => '9','attachable_id'=>'2','attachable_type'=>'project','title'=>'Project image','link'=>'https://image.freepik.com/free-vector/business-start-up-concept-illustration_107791-86.jpg?1','type'=>'photo',),
+         array('id' => '10','attachable_id'=>'3','attachable_type'=>'project','title'=>'Project image','link'=>'https://image.freepik.com/free-vector/business-start-up-concept-illustration_107791-86.jpg?1','type'=>'photo',),
+         array('id' => '11','attachable_id'=>'4','attachable_type'=>'project','title'=>'Project image','link'=>'https://image.freepik.com/free-vector/business-start-up-concept-illustration_107791-86.jpg?1','type'=>'photo',),
+         array('id' => '12','attachable_id'=>'5','attachable_type'=>'project','title'=>'Project image','link'=>'https://image.freepik.com/free-vector/business-start-up-concept-illustration_107791-86.jpg?1','type'=>'photo',),
+         array('id' => '13','attachable_id'=>'6','attachable_type'=>'project','title'=>'Project image','link'=>'https://image.freepik.com/free-vector/business-start-up-concept-illustration_107791-86.jpg?1','type'=>'photo',),
+         array('id' => '14','attachable_id'=>'7','attachable_type'=>'project','title'=>'Project image','link'=>'https://image.freepik.com/free-vector/business-start-up-concept-illustration_107791-86.jpg?1','type'=>'photo',),
+         array('id' => '15','attachable_id'=>'8','attachable_type'=>'project','title'=>'Project image','link'=>'https://image.freepik.com/free-vector/business-start-up-concept-illustration_107791-86.jpg?1','type'=>'photo',),
+         array('id' => '16','attachable_id'=>'9','attachable_type'=>'project','title'=>'Project image','link'=>'https://image.freepik.com/free-vector/business-start-up-concept-illustration_107791-86.jpg?1','type'=>'photo',),
+         array('id' => '17','attachable_id'=>'10','attachable_type'=>'project','title'=>'Project image','link'=>'https://image.freepik.com/free-vector/business-start-up-concept-illustration_107791-86.jpg?1','type'=>'photo',),
+         array('id' => '18','attachable_id'=>'11','attachable_type'=>'project','title'=>'Project image','link'=>'https://image.freepik.com/free-vector/business-start-up-concept-illustration_107791-86.jpg?1','type'=>'photo',),
         );
         DB::table('attachments')->insert($attachments); 
        }
